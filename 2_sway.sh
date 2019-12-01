dcaa84faa191e8cafcdaa72c2480ca1d0c405cb4
exah-io <3874515+exah-io@users.noreply.github.com>
2019-12-01 22:33:50 +0000

    Rofi: Ayu Mirage

--- 2_sway.sh
+++ 2_sway.sh
@@ -55,5 +55,6 @@ wget -P ~/.config/alacritty https://raw.githubusercontent.com/exah-io/minimal-ar
 echo "Ricing rofi"
 mkdir -p ~/.config/rofi
 wget -P ~/.config/rofi https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/rofi/config
+wget -P ~/.config/rofi https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/rofi/ayu-mirage.rasi
 
 echo "Your setup is ready. You can reboot now!"
