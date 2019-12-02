cf32dc6b648f1d816d336a224df5334461b7cd37
exah-io <3874515+exah-io@users.noreply.github.com>
2019-12-02 01:30:42 +0000

    Add device-mapper to initial packages

--- 1_arch_install.sh
+++ 1_arch_install.sh
@@ -43,7 +43,7 @@ mount /dev/nvme0n1p1 /mnt/boot
 swapon /dev/mapper/Arch-swap
 
 echo "Installing Arch Linux"
-yes '' | pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 intel-ucode mesa networkmanager wget apparmor man-db man-pages nano vi diffutils
+yes '' | pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 device-mapper intel-ucode mesa networkmanager wget apparmor man-db man-pages nano vi diffutils
 
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
