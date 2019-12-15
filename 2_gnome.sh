401a8cb9212c304bb33603c7e2e66b1610ff66cf
exah-io <3874515+exah-io@users.noreply.github.com>
2019-12-15 19:43:20 +0000

    clean gnome script

--- 2_gnome.sh
+++ 2_gnome.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-user_name=""
-
 echo "Downloading and running base script"
 wget https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/2_base.sh
 chmod +x 2_base.sh
@@ -13,45 +11,12 @@ sudo pacman -S --noconfirm gnome gnome-tweaks gnome-usage gitg evolution gvfs-go
 echo "Enabling GDM"
 sudo systemctl enable gdm.service
 
-# echo "Changing Gnome power settings"
-# gsettings set org.gnome.desktop.screensaver lock-enabled 'true'
-# gsettings set org.gnome.desktop.screensaver lock-delay 600
-# gsettings set org.gnome.settings-daemon.plugins.power sleep-display-ac 600
-# gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 3600
-# gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type hibernate
-# gsettings set org.gnome.settings-daemon.plugins.power sleep-display-battery 600
-# gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 1800
-# gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type hibernate
-# gsettings set org.gnome.settings-daemon.plugins.power power-button-action suspend
-
-# echo "Changing battery level notifications and action"
-# sudo mkdir -p /etc/UPower/
-# sudo touch /etc/UPower/UPower.conf
-# sudo tee -a /etc/UPower/UPower.conf << END
-# PercentageLow=15
-# PercentageCritical=5
-# PercentageAction=2
-# CriticalPowerAction=HybridSleep
-# END
-
-# echo "Setting misc usability settings"
-# sudo -u gdm gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-# gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
-
-# echo "Changing Gnome fonts"
-# gsettings set org.gnome.desktop.interface text-scaling-factor '1.0'
-# gsettings set org.gnome.desktop.interface monospace-font-name "Hack 10"
-# gsettings set org.gnome.desktop.interface document-font-name 'Roboto 10'
-# gsettings set org.gnome.desktop.interface font-name 'Roboto 10'
-# gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Roboto Bold 10'
-# gsettings set org.gnome.nautilus.desktop font 'Roboto 10'
-
-# echo "Enabling automatic login"
-# sudo mkdir -p /etc/gdm/
-# sudo touch /etc/gdm/custom.conf
-# sudo tee -a /etc/gdm/custom.conf << END
-# # Enable automatic login for user
-# [daemon]
-# AutomaticLogin=$user_name
-# AutomaticLoginEnable=True
-# END
+echo "Enabling automatic login"
+sudo mkdir -p /etc/gdm/
+sudo touch /etc/gdm/custom.conf
+sudo tee -a /etc/gdm/custom.conf << END
+# Enable automatic login for user
+[daemon]
+AutomaticLogin=$USER
+AutomaticLoginEnable=True
+END
